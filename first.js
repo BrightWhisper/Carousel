c6f4c03c134ca8321c03dd0056ed2ec9bc645c85
wangshuyu <sureforwsy@163.com>
2019-05-17 22:15:59 +0800

    second

--- first.js
+++ first.js
@@ -1,21 +1,36 @@
-// $(p1).on('click', function(){
-//     $('.images').css({
-//         transform: 'translateX(0)'
-//     })
-// })
-// $(p2).on('click', function(){
-//     $('.images').css({
-//         transform: 'translateX(-300px)'
-//     })
-// })
-// $(p3).on('click', function(){
-//     $('.images').css({
-//         transform: 'translateX(-600px)'
-//     })
-// })
 let allButtons = $('button');
-for(let i=0;i<allButtons.length;i++){
-    $(allButtons[i]).on('click',function(x){
-        console.log(x.target);
+
+for (let i = 0; i < allButtons.length; i++) {
+    $(allButtons[i]).on('click', function (x) {
+        let index = $(x.currentTarget).index() - 1;
+        let p = index * -300;
+        $('.images').css({
+            transform: `translate(${p}px)`
+        })
+        n = index;
+        activeButton(allButtons.eq(n));
     })
 }
+
+let size = allButtons.length;
+let n = 0;
+playSlide(n%size);
+let timerId = setTimer();
+function setTimer(){
+    return setInterval(()=>{
+        n+=1;
+        playSlide(n%size);
+    },1000)
+}
+function playSlide(index){
+    allButtons.eq(index).trigger('click');
+}
+function activeButton($button) {
+    $button.addClass('red').siblings('.red').removeClass('red');
+}
+$('.window').on('mouseenter', function () {
+    window.clearInterval(timerId);
+})
+$('.window').on('mouseleave', function () {
+    timerId = setTimer();
+})
